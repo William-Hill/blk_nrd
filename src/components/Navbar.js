2b38a9382b18768a13c70c6eb2f06a1f31197246
William Hill <mjh2225@gmail.com>
2018-10-31 22:52:40 -0700

    added logo and social media links to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGhost } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import logo from '../img/Logo2.svg'
+
+library.add(fab)
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
@@ -9,29 +15,28 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={logo} alt="blk_nrd_logo" style={{ width: '350px', height: '400px' }} />
           </figure>
         </Link>
       </div>
       <div className="navbar-start">
+      </div>
+      <div className="navbar-end">
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
+        <div class="column">
+            <a href="https://www.facebook.com/theblknrd/?modal=admin_todo_tour"><FontAwesomeIcon className="social_icon" icon={['fab', 'facebook']} size="2x" color="black"/></a>
+        </div>
+        <div class="column">
+          <a href="https://www.instagram.com/theblknrd/?hl=en"><FontAwesomeIcon className="social_icon" icon={['fab', 'instagram']} size="2x" color="black"/></a>
+        </div>
+        <div class="column">
+          <a href="https://twitter.com/theblknrd"><FontAwesomeIcon className="social_icon" icon={['fab', 'twitter']} size="2x" color="black"/></a>
+        </div>
+        <div class="column">
+          <a href="https://www.youtube.com/channel/UCtrsF6jWS8_qG7mqwrD85JQ?view_as=subscriber"><FontAwesomeIcon className="social_icon" icon={['fab', 'youtube']} size="2x" color="black"/></a>
+        </div>
       </div>
     </div>
   </nav>
