e02fbdad6456a7a674afc909078696caa88423ca
William Hill <mjh2225@gmail.com>
2018-11-13 20:56:39 -0800

    removed unused imports

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGhost } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import logo from '../img/Logo2.svg'
 
