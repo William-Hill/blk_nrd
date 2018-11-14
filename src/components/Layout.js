d3624a49139c9a26c082c25bfe2f4da0be41d92b
William-Hill <mjh2225@gmail.com>
2018-11-13 23:10:13 -0800

    Revert "Styling blog"

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="TheBlkNrd" />
+    <Helmet title="Home | Gatsby + Netlify CMS" />
     <Navbar />
     <div>{children}</div>
   </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import logo from '../img/Logo2.svg'
-
-library.add(fab)
+import github from '../img/github-icon.svg'
+import logo from '../img/logo.svg'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
@@ -13,31 +9,29 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="blk_nrd_logo" style={{ width: '350px', height: '400px' }} />
+            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
           </figure>
         </Link>
       </div>
       <div className="navbar-start">
+        <Link className="navbar-item" to="/about">
+          About
+        </Link>
+        <Link className="navbar-item" to="/products">
+          Products
+        </Link>
       </div>
       <div className="navbar-end">
-        {/*
-          <Link className="navbar-item" to="/about">
-            About
-          </Link>
-          */
-        }
-        <div class="column">
-            <a href="https://www.facebook.com/theblknrd/?modal=admin_todo_tour"><FontAwesomeIcon className="social_icon" icon={['fab', 'facebook']} size="2x" color="black"/></a>
-        </div>
-        <div class="column">
-          <a href="https://www.instagram.com/theblknrd/?hl=en"><FontAwesomeIcon className="social_icon" icon={['fab', 'instagram']} size="2x" color="black"/></a>
-        </div>
-        <div class="column">
-          <a href="https://twitter.com/theblknrd"><FontAwesomeIcon className="social_icon" icon={['fab', 'twitter']} size="2x" color="black"/></a>
-        </div>
-        <div class="column">
-          <a href="https://www.youtube.com/channel/UCtrsF6jWS8_qG7mqwrD85JQ?view_as=subscriber"><FontAwesomeIcon className="social_icon" icon={['fab', 'youtube']} size="2x" color="black"/></a>
-        </div>
+        <a
+          className="navbar-item"
+          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <span className="icon">
+            <img src={github} alt="Github" />
+          </span>
+        </a>
       </div>
     </div>
   </nav>
