3831c54801df11cf3960d30785d9a15d047668b4
William Hill <mjh2225@gmail.com>
2018-11-17 10:10:09 -0800

    added semicolons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import logo from '../img/Logo1.svg'
+import React from 'react';
+import { Link } from 'gatsby';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import logo from '../img/Logo1.svg';
 
-library.add(fab)
+library.add(fab);
 
 export default class IndexPage extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <section class="hero is-fullheight has-bg-img">
-          <div class="hero-body">
-            <div class="container" id="main_content">
-              <div class="columns is-pulled-left">
-                <div class="column">
+        <section className="hero is-fullheight has-bg-img">
+          <div className="hero-body">
+            <div className="container" id="main_content">
+              <div className="columns is-pulled-left">
+                <div className="column">
                   <Link to="/" id="index_link" className="navbar-item">
                     <figure className="image">
                       <img id="blk_nrd_logo" src={logo} alt="blk_nrd" style={{ width: '400px', height: '400px' }} />
@@ -23,17 +23,17 @@ export default class IndexPage extends React.Component {
                   </Link>
                 </div>
             </div>
-              <div class="columns is-pulled-right">
-                <div class="column">
+              <div className="columns is-pulled-right">
+                <div className="column">
                     <a href="https://www.facebook.com/theblknrd/?modal=admin_todo_tour"><FontAwesomeIcon className="social_icon" icon={['fab', 'facebook']} size="3x" color="white"/></a>
                 </div>
-                <div class="column">
+                <div className="column">
                   <a href="https://www.instagram.com/theblknrd/?hl=en"><FontAwesomeIcon className="social_icon" icon={['fab', 'instagram']} size="3x" color="white"/></a>
                 </div>
-                <div class="column">
+                <div className="column">
                   <a href="https://twitter.com/theblknrd"><FontAwesomeIcon className="social_icon" icon={['fab', 'twitter']} size="3x" color="white"/></a>
                 </div>
-                <div class="column">
+                <div className="column">
                   <a href="https://www.youtube.com/channel/UCtrsF6jWS8_qG7mqwrD85JQ?view_as=subscriber"><FontAwesomeIcon className="social_icon" icon={['fab', 'youtube']} size="3x" color="white"/></a>
                 </div>
                 {/*
