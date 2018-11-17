774025256691ad6ade8307fa6ac3b10c52faadc0
William Hill <mjh2225@gmail.com>
2018-11-17 10:22:30 -0800

    added back Layout import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/Layout'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fab } from '@fortawesome/free-brands-svg-icons';
